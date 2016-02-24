ab02800990f7d848288bbb8525ba9edad352ef4d
Gordon Mickel <gordon.mickel@gmail.com>
2016-02-24 21:49:17 +0100

    add command validation

---
+++ src/lib/validCommands.js
@@ -0,0 +1,25 @@
+export default {
+  list: [
+    'lights',
+    'scenes',
+    'groups'
+  ],
+  group: [
+    'on',
+    'off',
+    'preset',
+    'bri',
+    'sat',
+    'hue',
+    'rgb'
+  ],
+  light: [
+    'on',
+    'off',
+    'preset',
+    'bri',
+    'sat',
+    'hue',
+    'rgb'
+  ]
+}
