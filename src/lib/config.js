2407b18fec3589f55cc8e0ef1a22c672253b4616
Gordon Mickel <gordon.mickel@gmail.com>
2016-02-26 12:49:06 +0100

    bridge discovery and user creation

--- src/lib/config.js
+++ src/lib/config.js
@@ -8,42 +8,34 @@ const configFileTemplate = `${configFile}.template`;
 
 let config;
 
-function checkRequiredConfigOptions(config) {
-  if (_.isEmpty(config.telegram.botToken)) {
-    throw new Error('Please enter the bot token in the config file');
-  }
-
-  if (_.isEmpty(config.bot.password)) {
-    throw new Error('Please enter a password for your bot in the config file');
-  }
-
-  if (_.isEmpty(config.hue.host)) {
-    logger.info('Hue Bridge host not specified, running Bridge discovery');
-    hugh.discoverBridges()
-      .then((bridges) => {
-        logger.info(`Found the following bridges, using the first one
+function discoverBridge() {
+  logger.info('Hue Bridge host not specified, running Bridge discovery');
+  return hugh.discoverBridges()
+    .then((bridges) => {
+      logger.info(`Found the following bridges, using the first one
         to use a different one, please edit your config.json.`);
-        config.hue.host = bridges[0].internalipaddress;
-        _.forEach(bridges, (bridge) => {
-          logger.info(`${bridge.internalipaddress}`);
-        })
-        return config;
-      })
-      .catch((error) => {
-        throw new Error(error.message);
+      config.hue.host = bridges[0].internalipaddress;
+      _.forEach(bridges, (bridge) => {
+        logger.info(`${bridge.internalipaddress}`);
       });
+    })
+}
 
-    if (_.isEmpty(config.hue.user)) {
-      // create user
-      logger.info('Hue Bridge user not specified, creating a user');
-    }
-  }
+function createUser() {
+  const hueApi = new hugh.HueApi(config.hue.host);
+  return hueApi.createUser({devicetype: 'telegram-hue-bot#node'})
+    .then((results) => {
+      config.hue.user = results[0].success.username;
+      logger.info(`${config.hue.user} created on the Hue bridge`);
+    })
+    .catch((error) => {
+      throw new Error(error.message);
+    })
 }
 
 try {
   logger.info('config file found %s', configFile);
   config = JSON.parse(fs.readFileSync(configFile, 'utf8'));
-  config = checkRequiredConfigOptions(config);
 } catch (err) {
 
   // JSON file is not correct
@@ -59,4 +51,40 @@ try {
   }
 }
 
+if (_.isEmpty(config.telegram.botToken)) {
+  throw new Error('Please enter the bot token in the config file');
+}
+
+if (_.isEmpty(config.bot.password)) {
+  throw new Error('Please enter a password for your bot in the config file');
+}
+
+if (_.isEmpty(config.hue.host) && _.isEmpty(config.hue.user)) {
+  discoverBridge()
+    .then(() => {
+      return createUser();
+    })
+    .then(() => {
+      // TODO: do write here
+      logger.info('config', config);
+    })
+    .catch((error) => {
+      process.nextTick(() => {
+        logger.error('Please press the link button on your hue Bridge and try again');
+        throw new Error(error.message);
+      });
+    })
+}
+
+/*
+if (_.isEmpty(config.hue.host)) {
+  discoverBridge();
+}
+
+if (_.isEmpty(config.hue.user)) {
+  discoverBridge();
+}
+*/
+
+// export something better
 export default config;
