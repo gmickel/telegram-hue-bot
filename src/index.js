34974579b4a1710ef7168fbc39fa29e9a9ab78b9
Gordon Mickel <gordon.mickel@gmail.com>
2016-03-01 21:51:45 +0100

    refactoring for keyboard mode and bug fixes - send commands back to chatId instead of fromId, allows the bot to be used properly in groups - start putting all text command matches into one block so we can have a text and keyboard command mode

--- src/index.js
+++ src/index.js
@@ -30,9 +30,9 @@ function sendMessage(userId, message) {
 /*
  * handle removing the custom keyboard
  */
-function replyWithError(userId, err) {
+function replyWithError(userId, chatId, err) {
   logger.warn('user: %s message: %s', userId, err.message);
-  return bot.sendMessage(userId, `*Error:* ${err.message}`, {
+  return bot.sendMessage(chatId, `*Error:* ${err.message}`, {
     parse_mode: 'Markdown',
     reply_markup: {
       hide_keyboard: true
@@ -60,30 +60,23 @@ function isRevoked(userId) {
  * verify user can use the bot
  */
 function verifyUser(userId) {
-  if (_.some(acl.allowedUsers, { id: userId }) !== true) {
-    replyWithError(
-      userId,
-      new Error('You are not authorized to use this bot.\n`/auth [password]` to authorize.'));
-    return false;
-  }
-
-  return true;
+  return _.some(acl.allowedUsers, { id: userId }) === true;
 }
 
 /*
  * prompt for admin message
  */
-function promptOwnerConfig(userId) {
+function promptOwnerConfig(userId, chatId) {
   const message = [`Your User ID is: ${userId}`];
   message.push('Please add your User ID to the config file in the field labeled \'owner\'.');
   message.push('This will allow you to use the admin commands.');
   message.push('Please restart the bot once you have updated the config file.');
-  return bot.sendMessage(userId, message.join('\n'));
+  return bot.sendMessage(chatId, message.join('\n'));
 }
 
 /*
  * verify admin of the bot
- */
+
 function verifyAdmin(userId) {
   if (isAuthorized(userId)) {
     promptOwnerConfig(userId);
@@ -96,6 +89,7 @@ function verifyAdmin(userId) {
 
   return true;
 }
+ */
 
 /*
  * save access control list
@@ -135,7 +129,7 @@ function getTelegramName(user) {
 /*
  * Send Help Commands To chat
  */
-function sendCommands(fromId) {
+function sendCommands(fromId, chatId) {
   const response = [`Hi ${getTelegramName(fromId)}!`];
   response.push('Below is a list of commands:');
   response.push('\n*General commands:*');
@@ -158,7 +152,51 @@ function sendCommands(fromId) {
   response.push('`xy <0-255>` Set the hue x and y coordinates of a color in CIE color space of a group or light.');
   response.push('`rgb <255,255,255>` Set the colour using RGB of a group or light.');
 
-  return bot.sendMessage(fromId, response.join('\n'), { parse_mode: 'Markdown', selective: 2 });
+  return bot.sendMessage(chatId, response.join('\n'), { parse_mode: 'Markdown', selective: 2 });
+}
+
+function handleAuthorization(fromId, chatId, user, password) {
+  const message = [];
+
+  if (isAuthorized(fromId)) {
+    message.push('Already authorized.');
+    message.push('Type /start to begin.');
+    return sendMessage(chatId, message.join('\n'));
+  }
+
+  // make sure the user is not banned
+  if (isRevoked(fromId)) {
+    message.push('Your access has been revoked, you cannot reauthorize.');
+    message.push('Please reach out to the bot owner for support.');
+    logger.warn(`The revoked user: ${fromId} tried to reauthorize`);
+    return sendMessage(chatId, message.join('\n'));
+  }
+
+  if (password !== config.bot.password) {
+    logger.warn(`The user: ${fromId} entered an invalid password.`);
+    return replyWithError(fromId, chatId, new Error('Invalid password.'));
+  }
+
+  acl.allowedUsers.push(user);
+  updateACL();
+
+  if (acl.allowedUsers.length === 1) {
+    if (!config.bot.owner) {
+      promptOwnerConfig(fromId, chatId);
+    }
+  }
+
+  message.push('You have been authorized.');
+  message.push('Type /help, /quick or /hue');
+
+  return sendMessage(chatId, message.join('\n'));
+}
+
+function sendUnauthorizedMsg(userId, chatId) {
+  replyWithError(
+    userId,
+    chatId,
+    new Error('You are not authorized to use this bot.\n`/auth [password]` to authorize.'));
 }
 
 /*
@@ -172,63 +210,178 @@ bot.getMe()
     throw new Error(err);
   });
 
-/*
- * handle authorization
- */
-bot.onText(/\/auth (.+)/, (msg, match) => {
-  const fromId = msg.from.id;
-  const password = match[1];
-
-  const message = [];
-
-  if (isAuthorized(fromId)) {
-    message.push('Already authorized.');
-    message.push('Type /start to begin.');
-    return sendMessage(fromId, message.join('\n'));
-  }
-
-  // make sure the user is not banned
-  if (isRevoked(fromId)) {
-    message.push('Your access has been revoked, you cannot reauthorize.');
-    message.push('Please reach out to the bot owner for support.');
-    logger.warn(`The revoked user: ${fromId} tried to reauthorize`);
-    return sendMessage(fromId, message.join('\n'));
-  }
-
-  if (password !== config.bot.password) {
-    logger.warn(`The user: ${fromId} entered an invalid password.`);
-    return replyWithError(fromId, new Error('Invalid password.'));
-  }
-
-  acl.allowedUsers.push(msg.from);
-  updateACL();
-
-  if (acl.allowedUsers.length === 1) {
-    if (!config.bot.owner) {
-      promptOwnerConfig(fromId);
-    }
-  }
-
-  message.push('You have been authorized.');
-  message.push('Type /help, /quick or /hue');
-
-  return sendMessage(fromId, message.join('\n'));
-});
-
 bot.on('message', (msg) => {
   const user = msg.from;
   const fromId = msg.from.id;
   const message = msg.text;
+  const chatId = msg.chat.id;
+  let match = null;
 
-  const keyboardControls = new KeyboardControls(bot, user, config, cache);
+  /*
+   * handle authorization
+   */
+  if ((match = /^\/auth (.+)$/g.exec(message)) !== null) {
+    const password = match[1];
+    return handleAuthorization(fromId, chatId, user, password);
+  }
 
-  if (/^\/(?:h|H)ue?/g.test(message)) {
-    if (!verifyUser(fromId)) {
+  // Reject all unauthorized commands except for /auth
+  if (!verifyUser(fromId)) {
+    return sendUnauthorizedMsg(fromId, chatId);
+  }
+
+  /**
+   * matches quick command
+   */
+  if (/^\/quick$/g.test(message)) {
+    logger.info(`user: ${fromId}, message: sent \'/quick\' command`);
+
+    hueApi.getGroups().then((groupIds) => {
+      const markup = [['/all on', '/all off']];
+
+      _.forEach(groupIds, (groupId) => {
+        markup.push([`/g ${groupId} on`, `/g ${groupId} off`]);
+      });
+
+      const opts = {
+        reply_to_message_id: msg.message_id,
+        reply_markup: JSON.stringify({
+          keyboard: markup,
+          resize_keyboard: true
+        })
+      };
+      bot.sendMessage(chatId,
+        'Hue bot started\n\nEnter /hue for keyboard control or /help for a list of commands', opts);
+    });
+    return;
+  }
+
+  /*
+   * handle help command
+   */
+  if (/^\/help$/g.test(message)) {
+    logger.info(`user: ${fromId}, message: sent \'/help\' command`);
+    return sendCommands(fromId, chatId);
+  }
+
+  /**
+   * matches the list command
+   * valid resources are lights, groups and scenes
+   */
+  if ((match = /^\/(?:ls|list)? (.+)$/g.exec(message)) !== null) {
+    logger.info(`user: ${fromId}, sent ${match[0]}`);
+
+    if (validCommands.list.indexOf(match[1]) === -1) {
+      const error = new Error('Resource doesn\'t exist');
+      return replyWithError(fromId, error);
+    }
+
+    hueApi.list(match)
+      .then((lights) => {
+        sendMessage(chatId, lights);
+      });
+    return;
+  }
+
+  /**
+   * matches the all command
+   * used to easily manipulate group 0 (all lights)
+   */
+  if ((match = /^\/a(?:ll)? (.+)$/g.exec(message)) !== null) {
+    logger.info(`user: ${fromId}, sent ${match[0]}`);
+
+    const groupId = 0;
+    const command = match[1];
+
+    if (command) {
+      if (validCommands.group.indexOf(command) === -1) {
+        return replyWithError(fromId, chatId, new Error('Resource doesn\'t exist'));
+      }
+    }
+
+    hueApi.groups(groupId, command)
+      .then((groups) => {
+        sendMessage(chatId, groups);
+      })
+      .catch((error) => {
+        replyWithError(fromId, chatId, new Error(error));
+      });
+    return;
+  }
+
+  /**
+   * matches the group command
+   * used to manipulate groups
+   */
+  if ((match = /^\/g(?:roup)? (.+)$/g.exec(message)) !== null) {
+    logger.info(`user: ${fromId}, sent ${match[0]}`);
+
+    const [groupId, command, value] = match[1].split(' ');
+
+    if (!command) {
+      hueApi.group(groupId)
+        .then((group) => {
+          sendMessage(chatId, group);
+        });
       return;
     }
 
-    keyboardControls.sendResources();
-    return;
+    if (command) {
+      if (validCommands.group.indexOf(command) === -1) {
+        return replyWithError(fromId, chatId, new Error('Resource doesn\'t exist'));
+      }
+    }
+
+    hueApi.groups(groupId, command, value)
+      .then((message) => {
+        sendMessage(chatId, message);
+      })
+      .catch((error) => {
+        replyWithError(fromId, chatId, new Error(error));
+      });
+  }
+
+  /**
+   * matches the light command
+   * used to get the attributes of a light
+   */
+
+  if ((match = /^\/l(?:ight)? (.+)$/g.exec(message)) !== null) {
+    logger.info(`user: ${fromId}, sent ${match[0]}`);
+    const [lightId, command, value] = match[1].split(' ');
+
+    if (!command) {
+      hueApi.light(lightId)
+        .then((light) => {
+          sendMessage(chatId, light);
+        });
+      return;
+    }
+
+    if (command) {
+      if (validCommands.light.indexOf(command) === -1) {
+        return replyWithError(fromId, chatId, new Error('Resource doesn\'t exist'));
+      }
+    }
+
+    hueApi.lights(lightId, command, value)
+      .then((message) => {
+        sendMessage(chatId, message);
+      })
+      .catch((error) => {
+        replyWithError(fromId, chatId, new Error(error));
+      });
+  }
+
+  const keyboardControls = new KeyboardControls(bot, user, config, cache);
+
+  // Start keyboard controls, send the resources keyboard to the user
+  if (/^\/(?:h|H)ue?/g.test(message)) {
+    if (!verifyUser(fromId)) {
+      return sendUnauthorizedMsg(fromId, chatId);
+    }
+
+    return keyboardControls.sendResources();
   }
 
   const currentState = cache.get(`state${user.id}`);
@@ -236,179 +389,8 @@ bot.on('message', (msg) => {
 
   if (currentState === state.RESOURCE) {
     logger.info(message);
-    keyboardControls.sendList(message);
-    return;
+    return keyboardControls.sendList(message);
   }
 
   // next maybe return a message with the selected thing etc
-
-});
-
-/**
- * matches start command
- */
-bot.onText(/\/quick/, (msg) => {
-  const fromId = msg.from.id;
-  if (!verifyUser(fromId)) {
-    return;
-  }
-
-  hueApi.getGroups().then((groupIds) => {
-    const markup = [['/all on', '/all off']];
-
-    _.forEach(groupIds, (groupId) => {
-      markup.push([`/g ${groupId} on`, `/g ${groupId} off`]);
-    });
-
-    const chatId = msg.chat.id;
-    const opts = {
-      reply_to_message_id: msg.message_id,
-      reply_markup: JSON.stringify({
-        keyboard: markup,
-        resize_keyboard: true
-      })
-    };
-    bot.sendMessage(chatId, 'Hue bot started\n\nEnter /help for a list of commands', opts);
-  });
-});
-
-/*
- * handle help command
- */
-bot.onText(/^\/help/, (msg) => {
-  const fromId = msg.from.id;
-  if (!verifyUser(fromId)) {
-    return;
-  }
-
-  logger.info(`user: ${fromId}, message: sent \'/help\' command`);
-  sendCommands(fromId);
-});
-
-/**
- * matches the list command
- * valid resources are lights, groups and scenes
- */
-bot.onText(/^\/(?:ls|list)? (.+)/, (msg, match) => {
-  const fromId = msg.from.id;
-  logger.info(`user: ${fromId}, sent ${match[0]}`);
-  if (!verifyUser(fromId)) {
-    return;
-  }
-
-  if (validCommands.list.indexOf(match[1]) === -1) {
-    const error = new Error('Resource doesn\'t exist');
-    replyWithError(fromId, error);
-    return;
-  }
-
-  hueApi.list(match)
-    .then((message) => {
-      sendMessage(fromId, message);
-    });
-});
-
-/**
- * matches the all command
- * used to easily manipulate group 0 (all lights)
- */
-bot.onText(/^\/a(?:ll)? (.+)/, (msg, match) => {
-  const fromId = msg.from.id;
-  logger.info(`user: ${fromId}, sent ${match[0]}`);
-  if (!verifyUser(fromId)) {
-    return;
-  }
-
-  const groupId = 0;
-  const command = match[1];
-
-  if (command) {
-    if (validCommands.group.indexOf(command) === -1) {
-      replyWithError(fromId, new Error('Resource doesn\'t exist'));
-      return;
-    }
-  }
-
-  hueApi.groups(groupId, command)
-    .then((message) => {
-      sendMessage(fromId, message);
-    })
-    .catch((error) => {
-      replyWithError(fromId, new Error(error));
-    });
-});
-
-/**
- * matches the group command
- * used to manipulate groups
- */
-bot.onText(/^\/g(?:roup)? (.+)/, (msg, match) => {
-  const fromId = msg.from.id;
-  logger.info(`user: ${fromId}, sent ${match[0]}`);
-  if (!verifyUser(fromId)) {
-    return;
-  }
-
-  const [groupId, command, value] = match[1].split(' ');
-
-  if (!command) {
-    hueApi.group(groupId)
-      .then((message) => {
-        sendMessage(fromId, message);
-      });
-    return;
-  }
-
-  if (command) {
-    if (validCommands.group.indexOf(command) === -1) {
-      replyWithError(fromId, new Error('Resource doesn\'t exist'));
-      return;
-    }
-  }
-
-  hueApi.groups(groupId, command, value)
-    .then((message) => {
-      sendMessage(fromId, message);
-    })
-    .catch((error) => {
-      replyWithError(fromId, new Error(error));
-    });
-});
-
-/**
- * matches the light command
- * used to get the attributes of a light
- */
-
-bot.onText(/^\/l(?:ight)? (.+)/, (msg, match) => {
-  const fromId = msg.from.id;
-  logger.info(`user: ${fromId}, sent ${match[0]}`);
-  if (!verifyUser(fromId)) {
-    return;
-  }
-
-  const [lightId, command, value] = match[1].split(' ');
-
-  if (!command) {
-    hueApi.light(lightId)
-      .then((message) => {
-        sendMessage(fromId, message);
-      });
-    return;
-  }
-
-  if (command) {
-    if (validCommands.light.indexOf(command) === -1) {
-      replyWithError(fromId, new Error('Resource doesn\'t exist'));
-      return;
-    }
-  }
-
-  hueApi.lights(lightId, command, value)
-    .then((message) => {
-      sendMessage(fromId, message);
-    })
-    .catch((error) => {
-      replyWithError(fromId, new Error(error));
-    });
 });
