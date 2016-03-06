0fcca7f0d0c97dde64ad6672278b58d1bc74b3af
Gordon Mickel <gordon.mickel@gmail.com>
2016-03-06 10:46:24 +0100

    use friendly scene names add emphasis to the id field in messages

--- src/lib/messageBuilder.js
+++ src/lib/messageBuilder.js
@@ -2,13 +2,13 @@
 
 import _ from 'lodash';
 import validCommands from './validCommands';
-import { arrayToChunks } from './utils';
+import { arrayToChunks, createFriendlySceneName } from './utils';
 
 class MessageBuilder {
   lights(lightsObj) {
     return _.map(lightsObj, (light, id) => {
       return `*${light.name}*
-       *id*: ${id} | on: ${light.state.on ? 'yes' : 'no'}
+       *id*: \`${id}\` | on: ${light.state.on ? 'yes' : 'no'}
         *h*: ${light.state.hue} | *s*: ${light.state.sat} | *b*: ${light.state.bri}`;
     }).join('\n');
   }
@@ -19,7 +19,6 @@ class MessageBuilder {
       return [`${id} - ${light.name} | on: ${light.state.on ? 'yes' : 'no'}`];
     });
   }
-
 
   light(lightObj) {
     return `*${lightObj.name}*
@@ -34,7 +33,7 @@ class MessageBuilder {
 
   groups(groupsObj) {
     return _.map(groupsObj, (group, id) => {
-      return `*${group.name}* - id: ${id}
+      return `*${group.name}* - id: \`${id}\`
       lights: ${group.lights.join(',')}`;
     }).join('\n');
   }
@@ -55,7 +54,7 @@ class MessageBuilder {
 
   scenes(scenesObj) {
     return _.map(scenesObj, (scene, id) => {
-      return `*${scene.name}* - id: ${id}
+      return `*${createFriendlySceneName(scene.name)}* - id: \`${id}\`
       lights: ${scene.lights.join(',')}`;
     }).join('\n');
   }
@@ -63,7 +62,7 @@ class MessageBuilder {
   // TODO: this isn't really useful, maybe crop
   scenesKeyboard(scenesObj) {
     return _.map(scenesObj, (scene, id) => {
-      return [`${id} - ${scene.name} - lights: ${scene.lights.join(',')}`];
+      return [`${id} ${createFriendlySceneName(scene.name)} - l: ${scene.lights.join(',')}`];
     });
   }
 
@@ -101,12 +100,6 @@ class MessageBuilder {
       keyboard.push(Object.keys(config.hue.values[command]).map(key => key + '%'));
     }
 
-    return keyboard;
-  }
-
-  groupValuesKeyboard(command, config) {
-    const keyboard = [];
-    keyboard.push(Object.keys(config.hue.values[command]).map(key => key + '%'));
     return keyboard;
   }
 }
