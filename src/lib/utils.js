fd4089e884ff864683e1213f66d0d3398cbc2aab
Gordon Mickel <gordon.mickel@gmail.com>
2016-03-06 10:45:37 +0100

    add util function to create friendlier scene names

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,7 +1,19 @@
 import _ from 'lodash';
 
-export const arrayToChunks = function (array, chunkSize) {
+export function arrayToChunks(array, chunkSize) {
   return _.map(array, (item, index) => {
     return index % chunkSize === 0 ? array.slice(index, index + chunkSize) : null;
   }).filter(item => item);
-};
+}
+
+export function createFriendlySceneName(name) {
+  const index = name.search(/on\s\d.*/);
+  let friendlyName;
+  if (index  !== -1) {
+    friendlyName = name.substring(0, index);
+  } else {
+    return name;
+  }
+
+  return friendlyName;
+}
